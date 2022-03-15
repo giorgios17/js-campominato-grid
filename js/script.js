c4cea72704f9c02b1645faa7c78f3b7369f2f12e
giorgios17 <giosur@live.it>
2022-03-15 15:21:53 +0100

    numerUnico

--- js/script.js
+++ js/script.js
@@ -6,15 +6,16 @@ const grid = document.querySelector('.grid');
 const columns = 10;
 const rows = 10;
 const totalSquares = columns * rows;
-const numberUsed = [];
+const allNumberExtract = [];
 
 //creazione quadrati dentro la griglia
 for(let i=0; i < totalSquares; i++){
     const square = document.createElement('div');
     square.classList.add('square');
     grid.appendChild(square);
-    square.innerHTML = numeroRandomRange(1, 100);
-    
+    const numberExtract = numeroUnico(1, 100, allNumberExtract);
+    square.innerHTML = numberExtract;
+    allNumberExtract.push(numberExtract);
 
 //function click cambio colore
     square.addEventListener('click', function(){
@@ -28,6 +29,15 @@ function numeroRandomRange(min, max){
     const range = (max - min) + 1;
     const numeroRandom = Math.floor(Math.random()*range + min);
     return numeroRandom; 
+}
+
+//numer unico
+function numeroUnico (min, max, used){
+    let numeroUnico = numeroRandomRange(min, max);
+    while(used.includes(numeroUnico)){
+        numeroUnico = numeroRandomRange(min, max);
+    }
+    return numeroUnico;
 }
 
 
